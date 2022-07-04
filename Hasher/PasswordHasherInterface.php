504784c5055602c8b86ceb67e9959e64c6adf240
Nicholas <47761650+omatamix@users.noreply.github.com>
2022-07-04 00:34:33 -0500

    Add the password hasher interface.

---
+++ Hasher/PasswordHasherInterface.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * GNU Lesser General Public License v2.1
+ *
+ * Copyright (c) 2022 Nicholas English
+ *
+ * Primarily used for software libraries, the GNU LGPL requires that derived works be licensed under the same
+ * license, but works that only link to it do not fall under this restriction. There are two commonly used
+ * versions of the GNU LGPL.
+ *
+ * @author <https://github.com/omatamix> Nicholas English
+ */
+
+namespace Vixel\Hasher;
+
+use InvalidArgumentException;
+
+interface PasswordHasherInterface
+{
+    /**
+     * Compute a new hash.
+     *
+     * @param string $password The password to hash.
+     *
+     * @throws \InvalidArgumentException If the password suppied is too long.
+     *
+     * @return string Returns the hashed password.
+     */
+    public function compute(string $password): string;
+
+    /**
+     * Verify the password matches the hash provided.
+     *
+     * @param string $password The password check.
+     * @param string $hash     The hash to check against.
+     *
+     * @return bool Returns true if the password matches the given hash else return false.
+     */
+    public function verify(string $password, string $hash): bool;
+
+    /**
+     * Determine if the hash needs a rehash.
+     *
+     * @param string $hash The hash to check.
+     *
+     * @return bool Returns true if the hash needs a rehash and false if not.
+     */
+    public function needsRehash(string $hash): bool;
+}
